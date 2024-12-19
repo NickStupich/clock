7ee5a217be739cf9aeb385f21f03f05da14a276d
Nick Stupich <nick.stupich@prolucid.ca>
2024-12-18 16:23:42 -0800

    cleaned up, but super laggy

--- BokehApp.py
+++ BokehApp.py
@@ -4,10 +4,14 @@ from bokeh.server.server import Server
 from bokeh.application import Application
 from bokeh.application.handlers.function import FunctionHandler
 from bokeh.plotting import figure, ColumnDataSource
+from bokeh.models import Plot, Range1d
 from threading import Thread
+
+import DrawClock
 
 class BokehApp():
     plot_data = []
+    hand_angles = DrawClock.clock_positions_base
     last_data_length = None
 
     stop_data_thread = False
@@ -36,20 +40,32 @@ class BokehApp():
     def startDataAcquisition(self):
         while not self.stop_data_thread:
             self.plot_data.append({'x': [random.random()], 'y': [random.random()], 'color': [random.choice(['red', 'blue', 'green'])]})
+            self.hand_angles[0][0][0] += 0.1
+            print('moved hands')
             time.sleep(0.1)
         print('data thread is done!')
 
+    update_count = 0
     def make_document(self, doc):
-        source = ColumnDataSource({'x': [], 'y': [], 'color': []})
-        fig = figure(title = 'Streaming Circle Plot!', sizing_mode = 'scale_both')
-        fig.circle(source = source, x = 'x', y = 'y', color = 'color', size = 10)
+        # source = ColumnDataSource({'x': [], 'y': [], 'color': []})
+        # fig = figure(title = 'Streaming Circle Plot!', sizing_mode = 'scale_both')
+        # fig.circle(source = source, x = 'x', y = 'y', color = 'color', size = 10)
+
+
+        plot = DrawClock.create_plot()
 
         def update():
-            if self.last_data_length is not None and self.last_data_length != len(self.plot_data):
-                source.stream(self.plot_data[-1])
-            self.last_data_length = len(self.plot_data)
+            print('updating[%d]...' % self.update_count)
+            # if self.last_data_length is not None and self.last_data_length != len(self.plot_data):
+            #     source.stream(self.plot_data[-1])
+            # self.last_data_length = len(self.plot_data)
 
-        doc.add_root(fig)
+            DrawClock.draw_full_clock(plot, self.hand_angles)
+            print('done[%d]' % self.update_count)
+
+            self.update_count+=1
+
+        doc.add_root(plot)
         doc.add_periodic_callback(update, 100)
 
 if __name__ == '__main__':
--- DrawClock.py
+++ DrawClock.py
@@ -3,14 +3,41 @@ from bokeh.plotting import figure, show
 from bokeh.models import AnnularWedge, ColumnDataSource, Grid, LinearAxis, Plot, Rect, Circle
 from bokeh.models import Range1d
 
+import datetime
+
+clock_positions_base = np.pi * np.array([
+                [(0,0),     (1, 1.5),   (0, 1.5, ),     (1, 1.5),       (1.5, 1.5),     (1.5, 1.5),     (0, 1.5),   (1, 1.5)    ],
+                [(0,1.5),   (1, 0.5),   (0.5, 1.5,),    (0.5, 1.5),     (0, 0.5),       (0.5, 1.5),     (0, 0.5),   (0.5, 1.5)  ],
+                [(0,0.5),   (1, 1),     (0, 0.5, ),     (0.5, 1),       (1.25, 1.25),   (0.5, 0.5),     (0, 0),     (0.5, 1)    ],
+            ])
 
 n_rows = 3
 n_cols = 8
 
-def draw_single_clock(center, angles):
+if 1:
+    border_padding = 50
+    distance_between_clocks = 300
+    border_outline_padding = 20
+
     hand_length = 130
     hand_width = 20
     clock_radius = 140
+
+    plot_width = 2500
+    plot_height = 1000 #dimensions
+else:
+    border_padding = 25
+    distance_between_clocks = 150
+    border_outline_padding = 10
+
+    hand_length = 65
+    hand_width = 10
+    clock_radius = 70
+
+    plot_width = 1250
+    plot_height = 500
+
+def draw_single_clock(plot, center, angles):
 
     hand_color = "#cab2d6"
 
@@ -19,8 +46,9 @@ def draw_single_clock(center, angles):
     hour_rect_center = (center[0] + np.cos(angle_hour) * hand_length/2, center[1] + np.sin(angle_hour) * hand_length/2)
     minute_rect_center = (center[0] + np.cos(angle_minute) * hand_length/2, center[1] + np.sin(angle_minute) * hand_length / 2)
 
-    glyph = Circle(x=center[0], y=center[1], radius=clock_radius, line_color="#3288bd", fill_color="#eeeeee", line_width=1)
-    plot.add_glyph(glyph)
+    
+    # glyph = Circle(x=center[0], y=center[1], radius=clock_radius, line_color="#3288bd", fill_color="#eeeeee", line_width=1)
+    # plot.add_glyph(glyph)
 
     glyph = Rect(x=hour_rect_center[0], y=hour_rect_center[1], width=hand_length, height=hand_width, angle=angle_hour, fill_color=hand_color)
     plot.add_glyph(glyph)
@@ -28,15 +56,14 @@ def draw_single_clock(center, angles):
     glyph = Rect(x=minute_rect_center[0], y=minute_rect_center[1], width=hand_length, height=hand_width, angle=angle_minute, fill_color=hand_color)
     plot.add_glyph(glyph)
 
-    glyph = Circle(x=center[0], y=center[1], radius=hand_width, fill_color=hand_color, line_width=1)
-    plot.add_glyph(glyph)
+    # glyph = Circle(x=center[0], y=center[1], radius=hand_width, fill_color=hand_color, line_width=1)
+    # plot.add_glyph(glyph)
+    
 
+def draw_full_clock(plot, angles):
+    start = datetime.datetime.now()
 
-def draw_full_clock(angles):
-
-    border_padding = 50
-    distance_between_clocks = 300
-    border_outline_padding = 20
+    plot.renderers = []
 
     #total required dimensions
     # print(border_padding*2 + distance_between_clocks * n_rows)
@@ -49,28 +76,30 @@ def draw_full_clock(angles):
     h = (border_padding * 2 + distance_between_clocks * (n_rows)) - 2 * border_outline_padding
 
     glyph = Rect(x=x, y=y, width=w, height=h, fill_color="#dddddd")
-
     plot.add_glyph(glyph)
     
     for row in range(n_rows):
         for col in range(n_cols):
             center = (border_padding + (col + 0.5) * distance_between_clocks, border_padding + (row + 0.5) * distance_between_clocks)
-            draw_single_clock(center, angles[n_rows - row -1 ,col]) #plotting is bottom up
+            draw_single_clock(plot, center, angles[n_rows - row -1 ,col]) #plotting is bottom up
 
+    print('full draw time: ', (datetime.datetime.now() - start))
+
+
+def create_plot():
+    plot = Plot(
+            title=None, width=plot_width, height=plot_height,
+            min_border=0, toolbar_location=None,
+            x_range=Range1d(0, plot_width), y_range=Range1d(0, plot_height))
+
+    return plot
 
 if __name__ == "__main__":
 
-    plot = Plot(
-        title=None, width=2500, height=1000,
-        min_border=0, toolbar_location=None,
-        x_range=Range1d(0, 2500), y_range=Range1d(0, 1000))
+    plot2 = create_plot()
 
     #time = 20:49
-    angles = np.pi * np.array([
-                [(0,0),     (1, 1.5),   (0, 1.5, ),     (1, 1.5),       (1.5, 1.5),     (1.5, 1.5),     (0, 1.5),   (1, 1.5)    ],
-                [(0,1.5),   (1, 0.5),   (0.5, 1.5,),    (0.5, 1.5),     (0, 0.5),       (0.5, 1.5),     (0, 0.5),   (0.5, 1.5)  ],
-                [(0,0.5),   (1, 1),     (0, 0.5, ),     (0.5, 1),       (1.25, 1.25),   (0.5, 0.5),     (0, 0),     (0.5, 1)    ],
-            ])
+    angles = clock_positions_base
 
-    draw_full_clock(angles)
-    show(plot)
+    draw_full_clock(plot2, angles)
+    show(plot2)
