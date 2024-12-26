9ba26bdaee412d940436fc99bf2b34622956af17
Nick Stupich <nick.stupich@gmail.com>
2024-12-25 20:55:58 -0800

    uart -> spi. working better, not perfect

--- Simulator/ArduinoInterface.py
+++ Simulator/ArduinoInterface.py
@@ -14,17 +14,22 @@ if test_environment:
 			pass
 else:
     
-	import serial
+	import spidev
 	import numpy as np
 
 	class ArduinoInterface(object):
 		def __init__(self, clock_hand_controller):
 			self.chc = clock_hand_controller
-			self.ser = serial.Serial('/dev/serial0', baudrate=9600)
+			#self.ser = serial.Serial('/dev/serial0', baudrate=9600)
+			self.spi = spidev.SpiDev()
+			self.spi.open(0,0)
+
+			self.spi.max_speed_hz = 10000
+
 
 		def transmitTargetPositions(self, target_angles):
 			print(target_angles.astype('int16'), target_angles.shape)
-			to_send = np.ascontiguousarray(target_angles, dtype='>i2').tobytes() + b'\xff\xff'
+			to_send = np.ascontiguousarray(target_angles, dtype='>i2').tobytes() + b'\xff\xff\xff'
 			# print('sending: ', to_send)
 
 			#to_send = to_send[0,:,:].tobytes()
@@ -33,8 +38,8 @@ else:
 
 			print(to_send, len(to_send))
 
-
-			self.ser.write(to_send)
+			response = self.spi.xfer2(to_send)
+			print(response, len(response))
 		#on changed state from arduino gpios:
 		#self.chc.updateClockState(disabled=True)
 
@@ -44,5 +49,6 @@ if __name__ == "__main__":
     import numpy as np
     #x = np.array([1, 2, 3, 4], dtype='int')
     x = np.ones((3, 8, 2), dtype='int')
+    x[0,0,0] = 1000
     x[1,1,1] = 42
     ai.transmitTargetPositions(x)
