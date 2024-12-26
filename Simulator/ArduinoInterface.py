0db0a8757381366d114b02762568c638e58b4b64
Nick Stupich <nick.stupich@gmail.com>
2024-12-25 21:05:51 -0800

    separate chip select for spi interface - much better

--- Simulator/ArduinoInterface.py
+++ Simulator/ArduinoInterface.py
@@ -1,3 +1,5 @@
+import time
+
 try:
 	import RPi.GPIO as gpio
 	test_environment = False
@@ -22,14 +24,17 @@ else:
 			self.chc = clock_hand_controller
 			#self.ser = serial.Serial('/dev/serial0', baudrate=9600)
 			self.spi = spidev.SpiDev()
-			self.spi.open(0,0)
+			self.spi.open(0,1)
+			gpio.setmode(gpio.BCM)
+			gpio.setup(8, gpio.OUT)
+			gpio.output(8, gpio.HIGH)
 
 			self.spi.max_speed_hz = 10000
 
 
 		def transmitTargetPositions(self, target_angles):
 			print(target_angles.astype('int16'), target_angles.shape)
-			to_send = np.ascontiguousarray(target_angles, dtype='>i2').tobytes() + b'\xff\xff\xff'
+			to_send = np.ascontiguousarray(target_angles, dtype='>i2').tobytes() + b'\xff\xff'
 			# print('sending: ', to_send)
 
 			#to_send = to_send[0,:,:].tobytes()
@@ -37,8 +42,12 @@ else:
 			#send3 = to_send[2,:,:].tobytes()
 
 			print(to_send, len(to_send))
-
+            
+			gpio.output(8, gpio.LOW)
+			time.sleep(0.01)
 			response = self.spi.xfer2(to_send)
+			time.sleep(0.01)
+			gpio.output(8, gpio.HIGH)
 			print(response, len(response))
 		#on changed state from arduino gpios:
 		#self.chc.updateClockState(disabled=True)
