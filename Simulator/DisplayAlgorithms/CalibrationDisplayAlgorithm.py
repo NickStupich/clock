cd406c7dbf7443272bcc0defc3a46b7d065fe87c
Nick Stupich <nick.stupich@gmail.com>
2025-03-07 21:53:52 -0800

    do a full circle before the calibration state to make sure motors are engaged

--- Simulator/DisplayAlgorithms/CalibrationDisplayAlgorithm.py
+++ Simulator/DisplayAlgorithms/CalibrationDisplayAlgorithm.py
@@ -11,8 +11,8 @@ class CalibrationDisplayAlgorithm(BaseDisplayAlgorithm.BaseDisplayAlgorithm):
 
 	def updateHandPositions(self, h, m, s, target_hand_angles, new_move_hand_angles):
 		if self.needs_update:
-			target_hand_angles[:,:,0] = 0
-			target_hand_angles[:,:,1] = 180
+			target_hand_angles[:,:,0] = 360
+			target_hand_angles[:,:,1] = 540
 			new_move_hand_angles[:,:,:] = 1
 			self.needs_update = False
 
