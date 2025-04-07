4e19e56161bb8c52cdf21ecdbc5b668f11a8f7d0
Nick Stupich <nick.stupich@prolucid.ca>
2025-04-06 17:40:01 -0700

    merge ArduinoInterfaces

--- Simulator/ArduinoInterface.py
+++ Simulator/ArduinoInterface.py
@@ -8,79 +8,56 @@ MAX_I2C_WRITE_LEN = 28 #smbus limitation
 try:
 	import RPi.GPIO as gpio
 	test_environment = False
+	import smbus
 except (ImportError, RuntimeError):
 	test_environment = True
 
 
-if test_environment:
-	class ArduinoInterface(object):
-		def __init__(self, clock_hand_controller):
-			self.name = "Test Arduino Interface"
-
-		def transmitTargetPositions(self, target_angles, new_moves, hand_speeds):
-			hand_speed_integers = np.round(np.clip(hand_speeds, Constants.MAX_VELOCITY / 256.0, Constants.MAX_VELOCITY) / Constants.BASE_VELOCITY * Constants.BASE_VELOCITY_TRANSMIT_VALUE).astype('uint8')
-
-			print(hand_speed_integers)
-
-			# print(hand_speed_integers.tobytes())
-			pass
-
-		def set_offsets(self, new_offsets):
-			print(new_offsets)
-
-		def resetHandPositions(self):
-			pass
-else:
-    
-	import smbus
-
-	class ArduinoInterface(object):
-		def __init__(self, clock_hand_controller):
-			self.chc = clock_hand_controller
+class ArduinoInterface(object):
+	def __init__(self, clock_hand_controller):
+		self.chc = clock_hand_controller
+		if not test_environment:
 			self.i2c = smbus.SMBus(1)
-			self.name = "I2C Arduino Interface"
+		self.name = "Test Arduino Interface" if test_environment else "I2C Arduino Interface"
 
-		def set_offsets(self, new_offsets):
-			print('offset array: ', new_offsets)
-			full_values_bytes = list(np.ascontiguousarray(np.clip(-new_offsets * 10, -120, 120), dtype='<i1').tobytes())
-			print('offset bytes to send: ', full_values_bytes)
+	def send(self, index, values):
+		print('send: ', index, values)
+
+		if not test_environment:
 			try:
-				self.i2c.write_i2c_block_data(2, 10, full_values_bytes[:24]) #10 is the magic offset byte
-				self.i2c.write_i2c_block_data(2, 11, full_values_bytes[24:]) #11 is the magic offset byte
+		
+				self.i2c.write_i2c_block_data(2, index, values)
 			except Exception as e:
 				print('writing to i2c: ', e)
 
+	def set_offsets(self, new_offsets):
+		print('offset array: ', new_offsets)
+		full_values_bytes = list(np.ascontiguousarray(np.clip(-new_offsets * 10, -120, 120), dtype='<i1').tobytes())
+		print('offset bytes to send: ', full_values_bytes)
+		self.send(10, full_values_bytes[:24]) #10 is the magic offset byte
+		self.send(11, full_values_bytes[24:]) #11 is the magic offset byte
 
-		def transmitTargetPositions(self, target_angles, new_moves, hand_speeds):
-			full_values_bytes = list(np.ascontiguousarray(target_angles, dtype='<i2').tobytes())
-			hand_speed_integers =(np.clip(hand_speeds, Constants.MAX_VELOCITY / 256.0, Constants.MAX_VELOCITY) / Constants.BASE_VELOCITY * Constants.BASE_VELOCITY_TRANSMIT_VALUE).astype('uint8')
 
-			speed_bytes = (hand_speed_integers).tobytes()
-			send_indices = np.where(new_moves.flatten())[0]
+	def transmitTargetPositions(self, target_angles, new_moves, hand_speeds):
+		full_values_bytes = list(np.ascontiguousarray(target_angles, dtype='<i2').tobytes())
+		hand_speed_integers =(np.clip(hand_speeds, Constants.MAX_VELOCITY / 256.0, Constants.MAX_VELOCITY) / Constants.BASE_VELOCITY * Constants.BASE_VELOCITY_TRANSMIT_VALUE).astype('uint8')
 
-			if len(send_indices) > 0:
+		speed_bytes = (hand_speed_integers).tobytes()
+		send_indices = np.where(new_moves.flatten())[0]
 
-				full_encoded_send = []
-				for i in send_indices:
-					full_encoded_send.append(int(i))
-					full_encoded_send.append(speed_bytes[i])
-					full_encoded_send.append(full_values_bytes[i*2])
-					full_encoded_send.append(full_values_bytes[i*2+1])
+		if len(send_indices) > 0:
+
+			full_encoded_send = []
+			for i in send_indices:
+				full_encoded_send.append(int(i))
+				full_encoded_send.append(speed_bytes[i])
+				full_encoded_send.append(full_values_bytes[i*2])
+				full_encoded_send.append(full_values_bytes[i*2+1])
+			
+			for batch_index, batch_start in enumerate(range(0, len(full_encoded_send), MAX_I2C_WRITE_LEN)):
+				batch = full_encoded_send[batch_start: batch_start + MAX_I2C_WRITE_LEN]
+				self.send(batch_index, batch)
 				
-				for batch_index, batch_start in enumerate(range(0, len(full_encoded_send), MAX_I2C_WRITE_LEN)):
-					batch = full_encoded_send[batch_start: batch_start + MAX_I2C_WRITE_LEN]
-					try:
-						self.i2c.write_i2c_block_data(2, batch_index, batch)
-					except Exception as e:
-						print('writing to i2c: ', e)
-
-
-		def resetHandPositions(self):
-			try:
-				self.i2c.write_byte(2, 0x40)
-			except Exception as e:
-				print('writing byte to i2c: ', e)
-
 
 if __name__ == "__main__":
     ai = ArduinoInterface(None)
@@ -89,7 +66,8 @@ if __name__ == "__main__":
     x[0,0,0] = 1000
     x[1,1,1] = 42
     x[:,:,:] = 720
+    speeds = 30.2 * np.ones((3, 8, 2), dtype='int')
     newMoves = np.zeros((3,8,2), dtype='int')
     newMoves[:,:,:] = 1
     newMoves[0,2,1] = 1
-    ai.transmitTargetPositions(x, newMoves)
+    ai.transmitTargetPositions(x, newMoves, speeds)
